a16e4ef02ad6662817691e098237989ef72b3d32
ObaniGemini <robin@pi-et-ro.net>
2023-04-23 14:55:17 +0200

    'Shooting' boolean

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -8,6 +8,7 @@ const JOYSTICK_SHOOT_MIN_LENGTH = 0.6
 
 var move_dir : Vector2 = Vector2(0, 0)
 var shoot_dir : Vector2 = Vector2(0, 0)
+var shooting : bool = false
 
 func dbg_vec(vec):
 	return str(floorf(vec.x * 100)/100.0) + ", " + str(floorf(vec.y * 100)/100.0)
@@ -16,8 +17,9 @@ func _physics_process(delta):
 	$Debug/VBoxContainer/HBoxContainer/MoveDir.text = dbg_vec(move_dir)
 	$Debug/VBoxContainer/HBoxContainer2/ShootDir.text = dbg_vec(shoot_dir)
 	$Debug/VBoxContainer/HBoxContainer3/Pos.text = dbg_vec(position)
+	$Debug/VBoxContainer/HBoxContainer4/Shooting.text = str(shooting)
 	
-	rotation = Vector2(-shoot_dir.y, shoot_dir.x).angle()
+	rotation = shoot_dir.angle()
 	velocity = move_dir * SPEED;
 	
 	move_and_slide()
@@ -28,9 +30,13 @@ func joy_axis_move(value):
 	return value
 
 func joy_axis_shoot(value, original):
+	shooting = true
 	if abs(value) < JOYSTICK_SHOOT_THRESHOLD:
-		if shoot_dir.length() > JOYSTICK_SHOOT_MIN_LENGTH: return 0.0
-		else: return original
+		if shoot_dir.length() > JOYSTICK_SHOOT_MIN_LENGTH:
+			return 0.0
+		else:
+			shooting = false
+			return original
 	return value
 
 func _input(event):
@@ -41,25 +47,43 @@ func _input(event):
 		elif event.axis == JOY_AXIS_RIGHT_Y: shoot_dir.y = joy_axis_shoot(event.axis_value, shoot_dir.y)
 	else:
 		if event.is_action_pressed("left"): move_dir.x -= 1
-		if event.is_action_released("left"): move_dir.x += 1
+		elif event.is_action_released("left"): move_dir.x += 1
 		if event.is_action_pressed("right"): move_dir.x += 1
-		if event.is_action_released("right"): move_dir.x -= 1
+		elif event.is_action_released("right"): move_dir.x -= 1
 
 		if event.is_action_pressed("up"): move_dir.y -= 1
-		if event.is_action_released("up"): move_dir.y += 1
+		elif event.is_action_released("up"): move_dir.y += 1
 		if event.is_action_pressed("down"): move_dir.y += 1
-		if event.is_action_released("down"): move_dir.y -= 1
+		elif event.is_action_released("down"): move_dir.y -= 1
 
 
-		if event.is_action_pressed("aim_left"): shoot_dir.x -= 1
-		if event.is_action_released("aim_left"): shoot_dir.x += 1
-		if event.is_action_pressed("aim_right"): shoot_dir.x += 1
-		if event.is_action_released("aim_right"): shoot_dir.x -= 1
+		if event.is_action_pressed("aim_left"):
+			shooting = true
+			shoot_dir.x -= 1
+		elif event.is_action_released("aim_left"):
+			shoot_dir.x += 1
 		
-		if event.is_action_pressed("aim_up"): shoot_dir.y -= 1
-		if event.is_action_released("aim_up"): shoot_dir.y += 1
-		if event.is_action_pressed("aim_down"): shoot_dir.y += 1
-		if event.is_action_released("aim_down"): shoot_dir.y -= 1
+		if event.is_action_pressed("aim_right"):
+			shooting = true
+			shoot_dir.x += 1
+		elif event.is_action_released("aim_right"):
+			shoot_dir.x -= 1
+		
+		if event.is_action_pressed("aim_up"):
+			shooting = true
+			shoot_dir.y -= 1
+		elif event.is_action_released("aim_up"):
+			shoot_dir.y += 1
+		
+		if event.is_action_pressed("aim_down"):
+			shooting = true
+			shoot_dir.y += 1
+		elif event.is_action_released("aim_down"):
+			shoot_dir.y -= 1
+
+
+		if shoot_dir.length() < JOYSTICK_SHOOT_MIN_LENGTH:
+			shooting = false
 	
 	move_dir.x = clampf(move_dir.x, -1, 1)
 	move_dir.y = clampf(move_dir.y, -1, 1)
