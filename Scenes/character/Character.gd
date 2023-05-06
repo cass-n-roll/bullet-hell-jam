622cfa235f6f9e1d468758f2e2c82d946fa2af3e
Adrien Pouyet <Ricocotam@users.noreply.github.com>
2023-05-06 18:26:08 +0200

    shield now stops when picked up

--- Scenes/character/Character.gd
+++ Scenes/character/Character.gd
@@ -14,6 +14,7 @@ func _physics_process(_delta):
 	if $Shield.is_picked:
 		$Shield/CharacterBody2D.global_position = $Player/CharacterBody2D/ShieldPosition.global_position
 
+
 func _input(event):
 	if event.is_action_pressed("shoot"):
 		trigger_shoot()
--- Scenes/character/Shield.gd
+++ Scenes/character/Shield.gd
@@ -14,6 +14,8 @@ func _on_bullet_hit():
 	pass
 
 func get_picked_up():
+	$CharacterBody2D.linear_velocity = Vector2(0,0)
+	$CharacterBody2D.angular_velocity = 0
 	is_picked = true
 	$PickupTimer.start()
 
